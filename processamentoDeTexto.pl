340c5b718dbd01a77c41ca29cbb18cacaf940a82
João Lucas Mota <54906718+joaomota59@users.noreply.github.com>
2021-04-30 01:44:59 -0300

    número de poemas no arquivo

--- processamentoDeTexto.pl
+++ processamentoDeTexto.pl
@@ -1,12 +1,28 @@
+#Site que pode testar expressoes regulares em um texto: https://regex101.com/
 use strict;
 
-my $arquivo = '2020.2-poesias_machado_de_assis.txt';
+#my $arquivo = '2020.2-poesias_machado_de_assis.txt';
+my $arquivo = '2020.2-poesias_machado_de_assis.txt'; #diretorio do arquivo txt
 
-open(my $fh, '<:encoding(UTF-8)', $arquivo) 
-|| die "Não foi possível abrir o arquivo '$arquivo' $!";
+open(my $fh, "<" ,$arquivo) #abrir o arquivo txt no modo leitura '<'
+  or die "Nao foi possivel abrir o arquivo!\n";
 
+my $texto="";
 while (my $linha = <$fh>){
-    chomp $linha;
-    print "$linha\n";
+    $texto="$texto$linha";#quarda todo texto do arquivo em uma variavel 
 }
 
+#print $texto; #mostra o texto do arquivo lido
+
+close ($arquivo);#Fecha arquivo txt
+
+
+my @matches = ($texto =~ /[\w][\n]{3}[\w]/g); #pega todas ocorrencias da expressao regular no texto
+my $countPoemas = () = $texto =~ /[\w][\n]{3}[\w]/g;
+
+
+print @matches; #mostra todas as ocorrencias da expressao regular encontrada no texto
+
+my $string_len = length(@matches);
+
+print "Numero total de poemas: $countPoemas";
